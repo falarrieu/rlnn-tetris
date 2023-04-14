caa28f35d4155b4d1bfeb3f5b9c7906feead827e
Tyson O'Leary <tmoleary@colostate.edu>
2023-04-14 11:39:09 -0600

    Testing

--- server/horizon.py
+++ server/horizon.py
@@ -5,14 +5,16 @@ def horizon(board):
     bottom = [checkColumn(board, i,  -1) for i in range(board.width)]
     left = [checkRow(board, i,  1) for i in range(board.height)]
     right = [checkRow(board, i,  -1) for i in range(board.height)]
-    return {top:top,bottom:bottom,left:left,right:right}
+    return {'top':top,'bottom':bottom,'left':left,'right':right}
 
 def checkColumn(board, column, direction):
     for i in range(board.height):
         if board.filled(i * direction,column):
             return i
+    return board.height-1
 
 def checkRow(board, row, direction):
     for i in range(board.width):
         if board.filled(row, i * direction):
             return i
+    return board.width-1
--- server/main.py
+++ server/main.py
@@ -1,9 +1,12 @@
 from Board import Board
+from horizon import horizon
 
 def main():
     board = Board()
     board.testBoard()
     board.printBoard()
+
+    print(horizon(board))
 
 
 if __name__ == "__main__":
